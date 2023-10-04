e5c5b68ec43a90d4eeb1e693f4e2e5742efa7abf
Hernán Giraldo <hernan.giraldo.giraldo@gmail.com>
2023-10-04 18:02:44 -0400

    get all tcs

--- src/app/services/credit-cards.service.ts
+++ src/app/services/credit-cards.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {CreditCard} from "../models/credit-cards";
-import {environment} from "../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { CreditCard } from "../models/credit-cards";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
